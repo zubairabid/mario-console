bede942f8f0bbcf6134885c11cc03f5994260a53
zubairabid <zubairabid1999+github@gmail.com>
2018-10-23 18:45:00 +0530

    main is almost p e r f e c t

--- main.py
+++ main.py
@@ -2,30 +2,28 @@
 Main file of execution
 '''
 
+import time
+
+from colorama import init
+
 from util import NBInput
 from util import clear
 from util import keypress
 
-import sys
-import select
-
 from gamestate import Game
-
-import time
-from colorama import init
 
 import configs
 
 if __name__ == "__main__":
     '''
-    game loop, environment setup, etc. Happens here
+    game loop, environment setup, etc. happens here
     '''
 
     # setting up Colorama
     init()
 
-    # sets up keyboard input
-    keys = NBInput()
+    # sets up keyboard INPUT
+    KEYS = NBInput()
 
     ##########################
     # Set up the environment #
@@ -36,13 +34,13 @@ if __name__ == "__main__":
         # Start the actual game configs here #
         ######################################
 
-        # clear screen and enable non blocking input
+        # clear screen and enable non blocking INPUT
         clear()
-        keys.nbTerm()
+        KEYS.nbTerm()
         LEVEL = 0
 
         print("Choose level you want to play:\n0\t1\t2")
-        LEVEL = int(keys.getCh())
+        LEVEL = int(KEYS.getCh())
 
         if LEVEL > 2 or LEVEL < 0:
             print('Choice out of bounds. Resetting to 0')
@@ -50,76 +48,76 @@ if __name__ == "__main__":
 
         # Start a new Game with level
         # the screen used is a property of the game object
-        lives = configs.MAX_LIFE
+        LIVES = configs.MAX_LIFE
 
-        status = 0
-        points = 0
+        STATUS = 0
+        POINTS = 0
 
-        while lives > 0:
-            game = Game(LEVEL, configs.STD_TIME, lives)
+        while LIVES > 0:
+            GAME = Game(LEVEL, configs.STD_TIME, LIVES)
 
             # Level screen
-            game.levelScreen(LEVEL, lives, points)
-            keys.flush()
-            keys.getCh()
+            GAME.levelScreen(LEVEL, LIVES, POINTS)
+            KEYS.flush()
+            KEYS.getCh()
 
             # Game loop executes as time remains
-            while game.getTRemain() > 0:
+            while GAME.getTRemain() > 0:
 
-                # frame stores the time remaining at the start of rendering
+                # FRAME stores the time remaining at the start of rendering
                 # a frame, so as to calculate how long actually rendering it
                 # took, and to maintain a framerate
-                frame = game.getTRemain()
+                FRAME = GAME.getTRemain()
 
                 # repaint screen
-                game.repaint()
+                GAME.repaint()
 
-                # poll for input
-                input = ''
-                if keys.kbHit():
-                    input = keys.getCh()
+                # poll for INPUT
+                INPUT = ''
+                if KEYS.kbHit():
+                    INPUT = KEYS.getCh()
 
-                # process input
-                cin = keypress(input)
-                if cin == -1:
-                    status = 2
+                # process INPUT
+                CIN = keypress(INPUT)
+                if CIN == -1:
+                    STATUS = 2
                     break
 
                 # run game mechanics for each cycle
-                status = game.changeState(cin)
-                if status != 0:
+                STATUS = GAME.changeState(CIN)
+                if STATUS != 0:
                     break
 
-                # Clear input buffer to prevent delayed response
-                keys.flush()
+                # Clear INPUT buffer to prevent delayed response
+                KEYS.flush()
 
                 # Maintains some sort of framerate
-                while frame - game.getTRemain() < 0.1:
+                while FRAME - GAME.getTRemain() < 0.1:
                     continue
 
             # Game quit
-            if status == 2:
+            if STATUS == 2:
                 break
             # Level up
-            if status == 1:
-                status = 0
+            if STATUS == 1:
+                STATUS = 0
                 LEVEL += 1
-                points += game.points + game.getTRemain()//1
+                POINTS += GAME.points + GAME.getTRemain()//1
                 continue
 
             if LEVEL >= 3:
                 break
 
             # If here, death has happened.
-            lives -= 1
+            LIVES -= 1
 
     finally:
         time.sleep(0.5)
         clear()
-        print("GAME OVER. Final points: " + str(points) +
+        print("GAME OVER. Final points: " + str(POINTS) +
               "\nPress any key to exit")
 
-        keys.flush()
-        keys.getCh()
+        KEYS.flush()
+        KEYS.getCh()
         # Switch back to the original terminal state
-        keys.orTerm()
+        KEYS.orTerm()
