4e196dc1333649f84c6ad8b5e19e0539fcd158ca
zubairabid <zubairabid1999+github@gmail.com>
2018-10-29 20:43:25 +0530

    tests wrapups

--- 20171076_part2/refactored/tests/test_input.py
+++ 20171076_part2/refactored/tests/test_input.py
@@ -2,11 +2,25 @@ from util import keypress
 import pytest
 
 def test_keypress():
-    assert keypress('Q') == -1;
-    assert keypress('q') == -1;
-    assert keypress('W') == 3;
-    assert keypress('w') == 3;
-    assert keypress('A') == 2;
-    assert keypress('a') == 2;
-    assert keypress('D') == 1;
-    assert keypress('d') == 1;
+
+    for i in range(65536):
+        character = chr(i)
+
+        if character == 'Q':
+            assert keypress('Q') == -1;
+        elif character == 'q':
+            assert keypress('q') == -1;
+        elif character == 'W':
+            assert keypress('W') == 3;
+        elif character == 'w':
+            assert keypress('w') == 3;
+        elif character == 'A':
+            assert keypress('A') == 2;
+        elif character == 'a':
+            assert keypress('a') == 2;
+        elif character == 'D':
+            assert keypress('D') == 1;
+        elif character == 'd':
+            assert keypress('d') == 1;
+        else:
+            assert keypress(character) == 0
