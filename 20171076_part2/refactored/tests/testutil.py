35e8f77c7bdb3feaf15f916f480269aba38bacc0
zubairabid <zubairabid1999+github@gmail.com>
2018-10-29 18:33:47 +0530

    Tested movement, initialization

--- 20171076_part2/refactored/tests/testutil.py
+++ 20171076_part2/refactored/tests/testutil.py
@@ -1,18 +1,8 @@
-from gamestate import Game
-
-def test_game_init_with_proper_params():
-    '''
-    Checks for proper initialization of Game
-    '''
-
-    GAME = Game(0, 100, 1)
+def mario_exists_in_bounds(GAME):
     PLAYER_POS_I = GAME.player.i
     PLAYER_POS_J = GAME.player.j
     PLAYER_SIZE_I = GAME.player.get_size()[0]
     PLAYER_SIZE_J = GAME.player.get_size()[1]
-
-    # Check time allocated is correct
-    assert GAME.etime - GAME.stime == 100
 
     # Checking that Mario exists within bounds
     for i in range(PLAYER_POS_I+1-PLAYER_SIZE_I, PLAYER_POS_I+1):
